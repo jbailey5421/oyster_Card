791513ad17c2af9e13decbbbad90440c62175b5e
Jordan Bailey <jordanbailey201@gmail.com>
2018-10-12 08:30:20 +0000

    everything up to step 12 completed

---
+++ lib/oystercard.rb
@@ -0,0 +1,34 @@
+class Oystercard
+
+  MAXIMUM_BALANCE = 90
+  MINIMUM_CHARGE = 1
+
+  attr_reader :balance
+  attr_reader :entry_station
+  def initialize 
+    @balance = 0
+    @in_journey = false
+  end
+
+  def top_up(amount)
+    fail 'Maximum balance exceeded' if amount + balance > MAXIMUM_BALANCE
+    @balance += amount
+  end
+
+  def in_journey?
+    !!entry_station
+  end
+  def touch_in(station)
+   fail 'Insufficient balance to touch in' if balance < MINIMUM_CHARGE
+   @in_journey = true
+   @entry_station = station
+  end
+  def touch_out
+    deduct(MINIMUM_CHARGE)
+    @entry_station = nil
+  end
+end
+private 
+  def deduct(amount)
+    @balance -= amount
+  end
---
+++ spec/oystercard_spec.rb
@@ -0,0 +1,57 @@
+require 'oystercard'
+
+describe Oystercard do
+  let(:station){ double :station }
+ 
+ 
+ 
+ 
+ 
+  it 'has a balance of zero' do 
+    expect(subject.balance).to eq(0)
+  end
+  describe 'top_up' do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+    it 'can top up the balance' do 
+      expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+    end
+    it 'raises an error if the maximum balance is exceeded' do
+        maximum_balance = Oystercard::MAXIMUM_BALANCE
+        subject.top_up(maximum_balance)
+        expect{ subject.top_up 1 }.to raise_error 'Maximum balance exceeded'
+    end
+   
+    it 'is initially not in a journey' do
+      expect(subject).not_to be_in_journey
+    end
+    it "can touch in" do
+     subject.top_up(10)
+     subject.touch_in(station)
+     expect(subject).to be_in_journey
+    end
+    
+     
+    
+
+    it 'stores the entry station' do
+      subject.top_up(10)
+      subject.touch_in(station)
+      expect(subject.entry_station).to eq station
+    end
+    it "can touch out" do
+     subject.top_up(10)
+     subject.touch_in(station)
+     subject.touch_out
+     expect(subject).not_to be_in_journey
+    end
+    it 'will not touch in if below minimum balance' do
+     subject.top_up(0)
+     expect{ subject.touch_in(station) }.to raise_error "Insufficient balance to touch in"
+    end
+    it 'will make a charge on touch out' do
+     subject.top_up(10)
+     subject.touch_in(station)
+     expect{ subject.touch_out }.to change{ subject.balance }.by(-Oystercard::MINIMUM_CHARGE)
+    end
+  end
+end
